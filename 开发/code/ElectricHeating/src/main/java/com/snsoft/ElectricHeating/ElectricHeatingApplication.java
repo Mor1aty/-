95d029347ef21fded335bdc8a09b4f69302b70d6
Mor1aty <971896352@qq.com>
2019-03-25 14:46:17 +0800

    2019-3-25 代码初始化

---
+++ 开发/code/ElectricHeating/src/main/java/com/snsoft/ElectricHeating/ElectricHeatingApplication.java
@@ -0,0 +1,13 @@
+package com.snsoft.ElectricHeating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricHeatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricHeatingApplication.class, args);
+	}
+
+}
